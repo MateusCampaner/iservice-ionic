f87db0f7e5ee898af0efbf39c83f02227b1a222d
Mateus Campaner <82098812+MateusCampaner@users.noreply.github.com>
2021-07-05 20:30:34 -0300

    tabs 2

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  inputTexto = "";
+  marcacaoRadio = "";
 
-  constructor() {}
+  Limpar(){
+    this.inputTexto = "";
+    this.marcacaoRadio = ""; 
+  }
+
+  constructor(public alertController: AlertController) {}
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Serviço publicado com sucesso!',
+      message: 'Você pode ver mais detalhes no seu perfil.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+  }
 
 }
