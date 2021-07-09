b74ed30d1cfcccfbf7d9b7b4920f6298dc7ec99b
Mateus Campaner <82098812+MateusCampaner@users.noreply.github.com>
2021-07-09 11:10:13 -0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,20 +3,26 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: 'tab1',
+    loadChildren: () => import('./tab1/tab1.module').then(m => m.Tab1PageModule)
+  },
+  {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },  {
-    path: 'loading-page',
     loadChildren: () => import('./loading-page/loading-page.module').then( m => m.LoadingPagePageModule)
   },
   {
-    path: 'chat',
-    loadChildren: () => import('./chat/chat.module').then( m => m.ChatPageModule)
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'registrar',
+    loadChildren: () => import('./registrar/registrar.module').then( m => m.RegistrarPageModule)
   },
   {
     path: 'redefine',
     loadChildren: () => import('./redefine/redefine.module').then( m => m.RedefinePageModule)
   }
+
 
 ];
 @NgModule({
