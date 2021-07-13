720e22c863fac07e731851cc70b950cd5c4457ed
Mateus Campaner <82098812+MateusCampaner@users.noreply.github.com>
2021-07-13 10:10:51 -0300

    alert registro

--- src/app/registrar/registrar.page.ts
+++ src/app/registrar/registrar.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-registrar',
@@ -7,7 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrarPage implements OnInit {
 
-  constructor() { }
+  constructor(public alertController: AlertController) {}
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Perfil criado  com sucesso!',
+      message: 'Conecte-se para criar e oferecer serviços!.',
+      buttons: ['Voltar','OK']
+    });
+
+  
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+  }
 
   ngOnInit() {
   }
